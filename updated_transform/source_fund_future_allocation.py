ee5cda3a4126cebb7ffccd28567bbf7ac59d03b3
unknown <manish.yadav64mmmut@gmail.com>
2023-06-06 12:50:19 +0530

    made changes in Participant_balances removed extra columns

--- updated_transform/source_fund_future_allocation.py
+++ updated_transform/source_fund_future_allocation.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import *
 
 def transform(spark: SparkSession, primary_key: list):
 
-    nw00paap_nlarc_prdps_pdab_sffa_data_dat01_df = spark.sql("SELECT fund_number,plan_number,money_source as source,policy_number as participant_id,soc_sec_no as social_security_number,allocation_pct,system_code,company_code,process_date,'PREMIER' as source_system  FROM `nw00paap_nlarc_prdps_pdab_sffa_data`")
+    nw00paap_nlarc_prdps_pdab_sffa_data_dat01_df = spark.sql("SELECT fund_number,plan_number,money_source as source,policy_number as participant_id,soc_sec_no as social_security_number,allocation_pct,system_code,company_code,process_date,'PREMIER' as source_system  FROM nw00paap_nlarc_prdps_pdab_sffa_data ")
 
     transform_df = nw00paap_nlarc_prdps_pdab_sffa_data_dat01_df
 
